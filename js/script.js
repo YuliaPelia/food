e27f263eccf5da44e56d41d858e9cfa0f3accbfe
Yulchikss <yuliapelia19@gmail.com>
2023-06-19 21:35:25 +0300

    touch cwipe

--- js/script.js
+++ js/script.js
@@ -59,76 +59,76 @@ window.addEventListener("DOMContentLoaded", () => {
 
   slidesWrapper.append(indicators);
 
-  for (let i = 0; i < slides.length; i++) {
+  function createDot(i) {
     const dot = document.createElement("li");
     dot.setAttribute("data-slide-to", i + 1);
-
-    indicators.append(dot);
-    if (i == 0) {
-      dot.style.opacity = 1;
-    }
-    indicators.append(dot);
-    dots.push(dot);
+    return dot;
   }
 
-  if (slides.length < 10) {
-    total.textContent = `0${slides.length}`;
-    current.textContent = `0${slideIndex}`;
-  } else {
-    total.textContent = slides.length;
-    current.textContent = slideIndex;
-  }
-
-  slidesField.style.width = 100 * slides.length + "%";
-
-  slides.forEach((slide) => {
-    slide.style.width = width;
-  });
-
-  next.addEventListener("click", () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
-      offset = 0;
-    } else {
-      offset += +width.slice(0, width.length - 2);
-    }
-    slidesField.style.transform = `translateX(-${offset}px)`;
-    if (slideIndex == slides.length) {
-      slideIndex = 1;
-    } else {
-      slideIndex++;
-    }
-
+  function updateSlideIndexText() {
     if (slides.length < 10) {
       current.textContent = `0${slideIndex}`;
     } else {
       current.textContent = slideIndex;
     }
+  }
 
+  function updateDotOpacity() {
     dots.forEach((dot) => (dot.style.opacity = ".5"));
     dots[slideIndex - 1].style.opacity = 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = createDot(i);
+    indicators.append(dot);
+    if (i === 0) {
+      dot.style.opacity = 1;
+    }
+    dots.push(dot);
+  }
+
+  total.textContent = `0${slides.length}`;
+  updateSlideIndexText();
+
+  slidesField.style.width = 100 * slides.length + "%";
+  slides.forEach((slide) => {
+    slide.style.width = width;
   });
+
+  next.addEventListener("click", () => {
+    if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex === slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
+
+    updateSlideIndexText();
+    updateDotOpacity();
+  });
+
   prev.addEventListener("click", () => {
-    if (offset == 0) {
+    if (offset === 0) {
       offset = +width.slice(0, width.length - 2) * (slides.length - 1);
     } else {
       offset -= +width.slice(0, width.length - 2);
     }
     slidesField.style.transform = `translateX(-${offset}px)`;
 
-    if (slideIndex == 1) {
+    if (slideIndex === 1) {
       slideIndex = slides.length;
     } else {
       slideIndex--;
     }
 
-    if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
-
-    dots.forEach((dot) => (dot.style.opacity = ".5"));
-    dots[slideIndex - 1].style.opacity = 1;
+    updateSlideIndexText();
+    updateDotOpacity();
   });
 
   dots.forEach((dot) => {
@@ -139,16 +139,69 @@ window.addEventListener("DOMContentLoaded", () => {
       offset = +width.slice(0, width.length - 2) * (slideTo - 1);
 
       slidesField.style.transform = `translateX(-${offset}px)`;
-      if (slides.length < 10) {
-        current.textContent = `0${slideIndex}`;
-      } else {
-        current.textContent = slideIndex;
-      }
-
-      dots.forEach((dot) => (dot.style.opacity = ".5"));
-      dots[slideIndex - 1].style.opacity = 1;
+      updateSlideIndexText();
+      updateDotOpacity();
     });
   });
+  let touchStartX = 0;
+  let touchEndX = 0;
+
+  slidesField.addEventListener("touchstart", (e) => {
+    touchStartX = e.touches[0].clientX;
+  });
+
+  slidesField.addEventListener("touchend", (e) => {
+    touchEndX = e.changedTouches[0].clientX;
+    handleSwipe();
+  });
+
+  function handleSwipe() {
+    const minSwipeDistance = 50;
+
+    if (touchStartX - touchEndX > minSwipeDistance) {
+      // Свайп вліво
+      nextSlide();
+    } else if (touchEndX - touchStartX > minSwipeDistance) {
+      // Свайп вправо
+      prevSlide();
+    }
+  }
+
+  function nextSlide() {
+    if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex === slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
+
+    updateSlideIndexText();
+    updateDotOpacity();
+  }
+
+  function prevSlide() {
+    if (offset === 0) {
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+      offset -= +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex === 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    updateSlideIndexText();
+    updateDotOpacity();
+  }
 });
 
 new WOW().init();
