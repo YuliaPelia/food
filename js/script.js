6575c206186867a4da46b77aeb679a0d6051483a
Yulchikss <yuliapelia19@gmail.com>
2023-07-16 20:38:14 +0300

    feat: create timer

--- js/script.js
+++ js/script.js
@@ -398,6 +398,116 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   bindModal(".modal_btn", ".modal", ".modal .modal__close");
+
+  // timer
+
+  const deadline = "2023-12-11";
+
+  const getTimeRemaining = (endtime) => {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      t,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  };
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return "0" + num;
+    } else {
+      return num;
+    }
+  }
+  const setClock = (selector, endtime) => {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector("#days");
+    const hours = timer.querySelector("#hours");
+    const minutes = timer.querySelector("#minutes");
+    const seconds = timer.querySelector("#seconds");
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  };
+  setClock(".day_timer", deadline);
+
+  // const deadline = "2023-12-11";
+
+  // function getTimeRemaining(endtime) {
+  //   const t = Date.parse(endtime) - Date.parse(new Date()),
+  //     days = Math.floor(t / (1000 * 60 * 60 * 24)),
+  //     seconds = Math.floor((t / 1000) % 60),
+  //     minutes = Math.floor((t / 1000 / 60) % 60),
+  //     hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+
+  //   return {
+  //     // total: t,
+  //     // days: days,
+  //     // hours: hours,
+  //     // minutes: minutes,
+  //     // seconds: seconds,
+  //     t,
+  //     days,
+  //     hours,
+  //     minutes,
+  //     seconds,
+  //   };
+  // }
+
+  // function getZero(num) {
+  //   if (num >= 0 && num < 10) {
+  //     return "0" + num;
+  //   } else {
+  //     return num;
+  //   }
+  // }
+
+  // function setClock(selector, endtime) {
+  //   const timer = document.querySelector(selector),
+  //     days = timer.querySelector("#days"),
+  //     hours = timer.querySelector("#hours"),
+  //     minutes = timer.querySelector("#minutes"),
+  //     seconds = timer.querySelector("#seconds"),
+  //     timeInterval = setInterval(updateClock, 1000);
+
+  //   updateClock();
+
+  //   function updateClock() {
+  //     const t = getTimeRemaining(endtime);
+
+  //     days.innerHTML = getZero(t.days);
+  //     hours.innerHTML = getZero(t.hours);
+  //     minutes.innerHTML = getZero(t.minutes);
+  //     seconds.innerHTML = getZero(t.seconds);
+
+  //     if (t.total <= 0) {
+  //       clearInterval(timeInterval);
+  //     }
+  //   }
+  // }
+
+  // setClock(".day_timer", deadline);
 });
 
 new WOW().init();
